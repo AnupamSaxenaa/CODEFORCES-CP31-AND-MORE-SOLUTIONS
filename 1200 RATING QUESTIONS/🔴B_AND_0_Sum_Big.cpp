75ac85ed55df9f7cd43288514a78fa2083401f14
Anupam Saxena <saxenaanupam2004@gmail.com>
2025-11-20 17:11:46 +0530

    ok

---
+++ 1200 RATING QUESTIONS/🔴B_AND_0_Sum_Big.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//-------------------------------//
+//          MACROS               //
+//-------------------------------//
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl '\n'
+
+//-------------------------------//
+//          TYPEDEFS             //
+//-------------------------------//
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+//-------------------------------//
+//        CONSTANTS              //
+//-------------------------------//
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+//-------------------------------//
+//        DEBUG UTILS            //
+//-------------------------------//
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << endl
+#else
+#define dbg(x)
+#endif
+
+//-------------------------------//
+//       UTILITY FUNCTIONS       //
+//-------------------------------//
+ll gcdll(ll a, ll b) { return b ? gcdll(b, a % b) : a; }
+ll lcmll(ll a, ll b) { return (a / gcdll(a, b)) * b; }
+ll mod_add(ll a, ll b, ll m = MOD) { return (a + b) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return (a * b) % m; }
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+//-------------------------------//
+//          CUSTOM HASH          //
+//-------------------------------//
+struct CustomHash {
+static uint64_t splitmix64(uint64_t x) {
+x += 0x9e3779b97f4a7c15;
+x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+return x ^ (x >> 31);
+    }
+size_t operator()(uint64_t x) const {
+static const uint64_t FIXED_RANDOM = 
+chrono::steady_clock::now().time_since_epoch().count();
+return splitmix64(x + FIXED_RANDOM);
+    }
+};
+//-------------------------------//
+//           SOLVE               //
+//-------------------------------//
+void solve() {
+    // Your logic here
+    ll n,k;
+    cin>>n>>k;
+    ll ans = 1;
+    for (int i = 0; i < k; i++)
+       ans = ((ans%MOD)*(n%MOD))%MOD;
+    cout<<ans<<endl;
+    
+}
+
+//-------------------------------//
+//             MAIN              //
+//-------------------------------//
+int main() {
+    fastio;
+    int t = 1;
+    cin >> t; // uncomment if multiple test cases
+    while (t--) solve();
+    return 0;
+}
---
+++ 1200 RATING QUESTIONS/🔴B_M_arrays.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//-------------------------------//
+//          MACROS               //
+//-------------------------------//
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl '\n'
+
+//-------------------------------//
+//          TYPEDEFS             //
+//-------------------------------//
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+//-------------------------------//
+//        CONSTANTS              //
+//-------------------------------//
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+//-------------------------------//
+//        DEBUG UTILS            //
+//-------------------------------//
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << endl
+#else
+#define dbg(x)
+#endif
+
+//-------------------------------//
+//       UTILITY FUNCTIONS       //
+//-------------------------------//
+ll gcdll(ll a, ll b) { return b ? gcdll(b, a % b) : a; }
+ll lcmll(ll a, ll b) { return (a / gcdll(a, b)) * b; }
+ll mod_add(ll a, ll b, ll m = MOD) { return (a + b) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return (a * b) % m; }
+ll mod_pow(ll a, ll b, ll m = MOD)
+{
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+//-------------------------------//
+//          CUSTOM HASH          //
+//-------------------------------//
+struct CustomHash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+//-------------------------------//
+//           SOLVE               //
+//-------------------------------//
+void solve()
+{
+    // Your logic here
+    ll n, m;
+    cin >> n >> m;
+    vl a(n);
+    for (auto &x : a)
+        cin >> x;
+    vector<ll> v(m, 0);
+    for (int i = 0; i < n; i++)
+        v[a[i] % m]++;
+
+    // debug
+    // for(auto &x : v)
+    // {
+    //     cout<<x<<" ";
+    // }
+
+    int cnt = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (i == 0)
+        {
+            if (v[i] != 0)
+            {
+                cnt++;
+                v[i] = 0;
+            }
+        }
+
+        else
+        {
+
+            int currL = v[i];
+            int reqr = m - i;
+            int currR = v[reqr];
+            if (currL == 0)
+            {
+                cnt+=currR;
+                v[reqr]=0;
+                continue;
+            }
+            if (currL == currR || abs(currL - currR) == 1)
+                cnt++;
+            else
+               {
+                if(currL>currR){
+                    currL -= (currR+1);
+                    cnt+=currL+1;
+                }else{
+                    currR -= (currL+1);
+                    cnt+=currR+1;
+                }
+               }
+            v[i] = 0;
+            v[reqr] = 0;
+        }
+    }
+    cout << cnt << endl;
+}
+
+//-------------------------------//
+//             MAIN              //
+//-------------------------------//
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t; // uncomment if multiple test cases
+    while (t--)
+        solve();
+    return 0;
+}
